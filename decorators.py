715b24f41ce2e432b2ba599aa2e1f486f67094fb
Andreas Kanz <andreas@akanz.de>
2020-08-29 14:47:59 +0200

    add decorator

---
+++ decorators.py
@@ -0,0 +1,17 @@
+# Runtime decorator
+
+import functools
+from time import time
+
+
+def runtime(f):
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        time_start = time()
+        result = f(*args, **kwargs)
+        duration = time() - time_start
+        print(f"func:{f.__name__} args:{args}, {kwargs} took: {duration:.2f} sec")
+        return result
+
+    return wrapper
